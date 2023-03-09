01013a8f3c2db3aab8ea2e0b20109eab4e724d05
西澤未乗 <>
2023-03-09 20:49:45 +0900

    no message

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -32,18 +32,6 @@
                   @foreach($image->languague as $langs)
                     <small class="languagueList">{{ $langs->languague }}</small>
                   @endforeach
-                  <div class="functionLink">
-                    <p class="functionBtn" data-target="function{{$index}}">機能一覧</p>
-                    <div class="functionModal none" id="function{{$index}}">
-                      <h4>機能一覧</h4>
-                      <div class="functionContents">
-                        <p class="functionClose">×</p>
-                      @foreach($image->functions as $index => $function)
-                        <p class="functionName">{{ $index + 1 }}：{{ $function->name }}</p>
-                      @endforeach
-                      </div>
-                    </div>
-                  </div>
                 </div>
                 <div class="imageList">
                   @foreach($image->postdetails as $index => $detail)
